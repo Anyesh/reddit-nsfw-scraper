c1ca206c3927787dbb04cbd991f2b5add587a20e
Anyesh <sthacruz@gmail.com>
2021-04-20 08:27:20 +0545

    feat: Update to use cli args

--- start_scraper.py
+++ start_scraper.py
@@ -1,5 +1,15 @@
+import argh
+
 from config import ROOT_URL, host_config
 from src.Scraper import NSFWScraper
 
-nsfw_scraper = NSFWScraper(url=ROOT_URL, config=host_config)
-nsfw_scraper.start("hidori rose")
+
+@argh.arg("--title", "-t", help="Title to scrape the reddit.")
+@argh.arg("--max-pages", "-p", help="Max # pages to scrape.")
+def main(title: str = "hidori rose", max_pages: int = 5) -> None:
+    nsfw_scraper = NSFWScraper(url=ROOT_URL, config=host_config)
+    nsfw_scraper.start(query=title, max_pages=max_pages)
+
+
+if __name__ == "__main__":
+    argh.dispatch_command(main)
