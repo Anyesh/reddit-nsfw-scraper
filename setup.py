216c7c4d4f3169962f2bdea30186d65ffd7f85c5
Anyesh <sthacruz@gmail.com>
2021-04-24 16:06:39 +0545

    feat: Add setup and console script

---
+++ setup.py
@@ -0,0 +1,28 @@
+import sys
+
+from setuptools import find_packages, setup
+
+
+def read_file(name):
+    with open(name) as fd:
+        return fd.read()
+
+
+setup(
+    name="nsfw-scraper",
+    version="0.1.5",
+    description=("Reddit NSFW scraper"),
+    url="https://github.com/Anyesh/reddit-nsfw-scraper",
+    # download_url="https://github.com/Anyesh/reddit-nsfw-scraper",
+    author="Anish Shrestha",
+    author_email="hello@anyesh.xyz",
+    license="Public domain",
+    packages=find_packages(exclude=["tests"]),
+    install_requires=read_file("requirements.txt").splitlines(),
+    entry_points={
+        "console_scripts": ["nsfw-scraper=start_scraper:main"],
+    },
+    test_suite="nose.collector",
+    zip_safe=False,
+    keywords=["reddit", "nsfw", "scraper"],
+)
